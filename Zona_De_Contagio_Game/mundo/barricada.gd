cdd099d01e8cab6cc3502dd8f335e9655811ceac
Lucas Alexandre Seemund <seemund@alunos.utfpr.edu.br>
2024-08-18 18:25:15 -0300

    barricada na cena

--- Zona_De_Contagio_Game/mundo/barricada.gd
+++ Zona_De_Contagio_Game/mundo/barricada.gd
@@ -1,16 +1,34 @@
 extends StaticBody2D
 
-var health: int = 10  # Vida inicial do objeto
+@export var health: int = 100  # Vida inicial do objeto
 
 func _init() -> void:
 	add_to_group("Barricada")
-	
+
 func _ready():
-	pass
+	set_collision_layer_value(6, true)
+	set_collision_mask_value(3, true)
+	_atualiza_frame()
 
 func _on_zombie_hit(damage: int):
-	health -= damage
 	print(health)
+	if health <= 0:
+		health = 0
+		return
+	health -= damage
+	_atualiza_frame()
 	if health <= 0:
 		set_collision_layer_value(6, false)
 		set_collision_mask_value(3, false)
+
+func _atualiza_frame():
+	if health >= 100:
+		$AnimatedSprite2D.play("barricada100")
+	elif health >= 75:
+		$AnimatedSprite2D.play("barricada75")
+	elif health >= 50:
+		$AnimatedSprite2D.play("barricada50")
+	elif health >= 25:
+		$AnimatedSprite2D.play("barricada25")
+	elif health >= 0:
+		$AnimatedSprite2D.play("barricada0")
