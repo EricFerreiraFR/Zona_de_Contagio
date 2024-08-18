5dd4367e3f9d704f7e5ae5a8691e7f2ec5533ecb
Lucas Alexandre Seemund <seemund@alunos.utfpr.edu.br>
2024-08-18 17:00:29 -0300

    code barricada

--- Zona_De_Contagio_Game/mundo/barricada.gd
+++ Zona_De_Contagio_Game/mundo/barricada.gd
@@ -1,24 +1,16 @@
 extends StaticBody2D
 
-var health: int = 100  # Vida inicial do objeto
-var is_passable_by_zombie: bool = false
+var health: int = 10  # Vida inicial do objeto
 
 func _init() -> void:
 	add_to_group("Barricada")
 	
 func _ready():
-	$cs2d.set("disabled", false)
+	pass
 
 func _on_zombie_hit(damage: int):
 	health -= damage
 	print(health)
 	if health <= 0:
-		is_passable_by_zombie = true
-		$cs2d.set("disabled", true)
-
-func _on_body_entered(body: Node):
-	if body.is_in_group("Zombie") and is_passable_by_zombie:
-		pass
-	elif body.is_in_group("Player"):
-		$cs2d.set("disabled", false)
-	print(body)
+		set_collision_layer_value(6, false)
+		set_collision_mask_value(3, false)
