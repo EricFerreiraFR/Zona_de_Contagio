21bdd2153750ff106906cfd7a7d6b6cd1e3f28e5
Lucas Alexandre Seemund <seemund@alunos.utfpr.edu.br>
2024-09-13 18:31:13 -0300

    Tentando arrumar barra de vida

--- Zona_De_Contagio_Game/mundo/HUDVidaPontos.gd
+++ Zona_De_Contagio_Game/mundo/HUDVidaPontos.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 func _ready():
+	pass
 	var posit: Transform2D = Transform2D(0, Vector2(0,0))
 	posit.origin = Vector2(8, 5)
 	posit.x = Vector2(38.5, 0)
@@ -14,6 +15,8 @@ func update_score(score: int):
 	$ScoreLabel.text = "PONTOS: %d" % score
 
 func update_health(health: int):
+	$ProgressBar.value = clamp( health, 0, 1000)
+	pass
 	var aux = health / 1000.0
 	aux *= 38.5
 	aux = clamp( aux, 1.0, 38.5 )
