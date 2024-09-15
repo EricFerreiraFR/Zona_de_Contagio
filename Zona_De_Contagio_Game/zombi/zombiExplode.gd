7d2484510ece200ad1d0d6f814f1dd56ab8a9826
EricFerreiraFR <50562020+EricFerreiraFR@users.noreply.github.com>
2024-09-15 20:47:35 -0300

    add explode zombie

---
+++ Zona_De_Contagio_Game/zombi/zombiExplode.gd
@@ -0,0 +1,19 @@
+extends "res://zombi/zombi.gd"
+
+
+@onready var animationTree : AnimationPlayer = $AnimationPlayer
+
+func _init() -> void:
+	# Adiciona o zombi ao grupo "zombi"
+	add_to_group("Zombi")
+
+
+func _ready() -> void:
+	
+	animationTree.play("walk")
+
+func _on_hitPlayer_timeout():
+	animationTree.stop()
+	animationTree.play("explode")
+	_follow._on_zombie_hit(50)
+	
